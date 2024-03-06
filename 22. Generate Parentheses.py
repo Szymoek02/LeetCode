2fdb6cfc6fc3f7b0ec987679cb9dc3c7ab8b8c84
Szymoek02 <stajszczykszymon@gmail.com>
2024-03-06 22:43:03 +0100

    Update and rename 22. Generate Parentheses to 22. Generate Parentheses.py

--- 22. Generate Parentheses.py
+++ 22. Generate Parentheses.py
@@ -1,6 +1,20 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         res = []
+
+        def backtrack(open, closed, s):
+            #valid answer
+            if len(s) == n * 2: res.append(s)
+            #add open
+            if open < n: backtrack(open + 1, closed, s + "(")
+            #add closing
+            if cloes < open: backtrack(open, closed, s + ")")
+        
+        backtrack(0, 0)
+        return res
+    #--------------------------------------------------
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
         stack = []
 
         def backtrack(open, closed):
