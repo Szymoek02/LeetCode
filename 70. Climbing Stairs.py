75f0c70349e4ef5277343accac0d8d9aa7067d1f
Szymoek02 <stajszczykszymon@gmail.com>
2024-04-03 18:07:19 +0200

    Create 70. Climbing Stairs.py

---
+++ 70. Climbing Stairs.py
@@ -0,0 +1,37 @@
+# backtracing recursive tree
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1 or n == 0:
+            return 1
+
+        return self.climbStairs(n - 1) + self.climbStairs(n - 2)
+
+
+# memoization
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = {}
+        return self.dp(memo, n)
+
+    def dp(self, memo, n):
+        if n == 1 or n == 0:
+            return 1
+
+        if n not in memo:
+            memo[n] = self.dp(memo, n - 1) + self.dp(memo, n - 2)
+
+        return memo[n]
+
+# tabulation - skip
+
+# full dp approach
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        a, b = 0, 1
+        
+        for i in range(n):
+            temp = b
+            b = a + b
+            a = temp
+
+        return b
