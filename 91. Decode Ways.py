928266ae0b977e4b4447581946b0f6e87f33bb37
Szymoek02 <stajszczykszymon@gmail.com>
2024-04-21 20:14:55 +0200

    Update 91. Decode Ways.py

--- 91. Decode Ways.py
+++ 91. Decode Ways.py
@@ -1,3 +1,25 @@
+# TLE
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        res = 0
+        cache = {}
+        def bruteforce(i):
+            nonlocal res
+            if i == len(s):
+                res += 1
+                return
+            if s[i] == '0':
+                return
+
+
+            bruteforce(i + 1)
+            if (i + 1) < len(s) and (s[i] == '1' or (s[i] == '2' and s[i + 1] < '7')):
+                bruteforce(i + 2)
+        
+        bruteforce(0)
+        return res
+
+# Time: O(n) Space: O(n) 
 class Solution:
     def numDecodings(self, s: str) -> int:
         if not s or s[0] == '0':
