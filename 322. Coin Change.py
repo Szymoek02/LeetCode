4b87ec36622f04e9b2694604600eacaa2a82bbdc
Szymoek02 <stajszczykszymon@gmail.com>
2024-05-05 02:33:05 +0200

    Update 322. Coin Change.py

--- 322. Coin Change.py
+++ 322. Coin Change.py
@@ -15,3 +15,15 @@ class Solution:
 
         res = dfs(amount)
         return res if res != math.inf else -1
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [math.inf] * (amount + 1)
+        dp[0] = 0
+
+        for i in range(amount + 1):
+            for c in coins:
+                if c <= i:
+                    dp[i] = min(dp[i], 1 + dp[i - c])
+
+        return dp[-1] if dp[-1] != math.inf else -1
